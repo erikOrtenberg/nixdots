ae66a39b43837e0bbd16a720bb8b078f9457d876
Erik Örtenberg <erik.ortenberg@gmail.com>
2025-07-08 13:37:02 +0200

    added steam to configuration

--- configuration.nix
+++ configuration.nix
@@ -141,6 +141,14 @@
   #
   # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
   system.stateVersion = "25.05"; # Did you read the comment?
+  programs.steam = {
+    enable = true;
+    extest.enable = true;
+    protontricks.enable = true;
+    extraCompatPackages = with pkgs; [
+      proton-ge-bin
+    ];
+  };
 
 }
 
--- home.nix
+++ home.nix
@@ -67,20 +67,22 @@ in {
     interactiveShellInit = ''
       set fish_greeting # Disable greeting
     '';
-    plugins = [
-      # Enable a plugin (here grc for colorized command output) from nixpkgs
-      # { name = "grc"; src = pkgs.fishPlugins.grc.src; }
-      # Manually packaging and enable a plugin
-      # {
-      #   name = "z";
-      #   src = pkgs.fetchFromGitHub {
-      #     owner = "jethrokuan";
-      #     repo = "z";
-      #     rev = "e0e1b9dfdba362f8ab1ae8c1afc7ccf62b89f7eb";
-      #     sha256 = "0dbnir6jbwjpjalz14snzd3cgdysgcs3raznsijd6savad3qhijc";
-      #   };
-      # }
-    ];
+    shellAliases = {
+      #navigation shortcuts
+      "..." = "cd ../..";
+      #git aliases
+      "gst" = "git status";
+      "gad" = "git add .";
+      "grs" = "git reset";
+      "gcm" = "git commit -m";
+      "gsh" = "git stash";
+      "gsp" = "git stash pop";
+      "gch" = "git checkout";
+      "gbr" = "git branch";
+      "gme" = "git merge";
+      "gph" = "git push";
+      "gpl" = "git pull --rebase --autostash";
+    };
   };
 
   
