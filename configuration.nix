a426be94ad7b75d8de480a20caab750240d08bda
Erik Örtenberg <erik.ortenberg@gmail.com>
2025-07-26 01:32:59 +0200

    added docker group to kryddan

--- configuration.nix
+++ configuration.nix
@@ -58,7 +58,7 @@
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.kryddan = {
     isNormalUser = true;
-    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+    extraGroups = [ "wheel" "docker" ]; # Enable ‘sudo’ for the user.
     packages = with pkgs; [
       ranger
       htop
